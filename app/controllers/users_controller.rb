b1f682fd83202390dab10530e8d83b9e7794811e
Gwekkeo <gwendoline.cassand@gmail.com>
2018-07-31 15:25:33 +0200

    Ajout des 3 formulaires

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
+	def home
+	end
+
   def new
+  	@user = User.new
   end
 
   def create
+  	puts "POUR FORM HTML :"
+  	puts "POUR FORM_TAG :"
   	mon_user = User.new(username: params["username"], email: params["email"], bio: params["bio"])
   	mon_user.save
 
   	redirect_to "/"
   end
+
+  def create2
+		puts "POUR FORM_FOR"
+	  	@user = params["user"]["username"]
+	  	@mail = params["user"]["email"]
+	  	@bio = params["user"]["bio"]
+
+	  	mon_user = User.new(username: @user, email: @mail, bio: @bio)
+	  	mon_user.save
+
+	  	redirect_to "/"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root 'users#new'
-  post '/', to: 'users#create'
+	get '/', to: 'users#home'
+
+	get '/new', to: 'users#new'
+	post '/new', to: 'users#create'
+
+	get '/new2', to: 'users#new2'
+	post '/new2', to: 'users#create2'
+	
+	get '/new3', to: 'users#new3'
+	post '/new3', to: 'users#create2'
 end
