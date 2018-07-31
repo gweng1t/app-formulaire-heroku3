7e9bf523cc3f3d438eb76ad8909a512f674563c9
Gwekkeo <gwendoline.cassand@gmail.com>
2018-07-31 11:24:00 +0200

    ajout 2eme methode de form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
+  	mon_user = User.new(username: params["username"], email: params["email"], bio: params["bio"])
+  	mon_user.save
+
+  	redirect_to "/"
   end
 end
